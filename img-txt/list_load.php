7e10e29747b44df8dfad193b057432dd13d14b0d
joyh7680 <76725322+joyh7680@users.noreply.github.com>
2023-06-14 18:06:32 +0900

    Add files via upload
    엇갈려있는 형태

---
+++ img-txt/list_load.php
@@ -0,0 +1,60 @@
+<div class="item">
+    <a href="#">
+        <div class="img"><img src="../images/sub/story1.jpg" alt=""></div>
+        <div class="txt-wrap">
+        <p class="date">2023.06.07</p>
+        <div class="tit">리스트의 제목이 들어가는 영역입니다. 제목이 들어가는 영역입니다.</div>
+        <div class="desc">내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다.내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다...</div>
+    </div>
+    </a>
+</div>
+<div class="item">
+    <a href="#">
+        <div class="img"><img src="../images/sub/story2.jpg" alt=""></div>
+        <div class="txt-wrap">
+            <p class="date">2023.06.07</p>
+            <div class="tit">리스트의 제목이 들어가는 영역입니다. 제목이 들어가는 영역입니다.</div>
+            <div class="desc">내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다.내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다...</div>
+        </div>
+    </a>
+</div>
+<div class="item">
+    <a href="#">
+        <div class="img"><img src="../images/sub/story3.jpg" alt=""></div>
+        <div class="txt-wrap">
+            <p class="date">2023.06.07</p>
+            <div class="tit">리스트의 제목이 들어가는 영역입니다. 제목이 들어가는 영역입니다.</div>
+            <div class="desc">내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다.내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다...</div>
+        </div>
+    </a>
+</div>
+<div class="item">
+    <a href="#">
+        <div class="img"><img src="../images/sub/story4.jpg" alt=""></div>
+        <div class="txt-wrap">
+            <p class="date">2023.06.07</p>
+            <div class="tit">리스트의 제목이 들어가는 영역입니다. 제목이 들어가는 영역입니다.</div>
+            <div class="desc">내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다.내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다...</div>
+        </div>
+    </a>
+</div>
+<div class="item">
+    <a href="#">
+        <div class="img"><img src="../images/sub/story6.jpg" alt=""></div>
+        <div class="txt-wrap">
+            <p class="date">2023.06.07</p>
+            <div class="tit">리스트의 제목이 들어가는 영역입니다. 제목이 들어가는 영역입니다.</div>
+            <div class="desc">내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다.내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다...</div>
+        </div>
+    </a>
+</div>
+<div class="item">
+    <a href="#">
+        <div class="img"><img src="../images/sub/story5.jpg" alt=""></div>
+        <div class="txt-wrap">
+            <p class="date">2023.06.07</p>
+            <div class="tit">리스트의 제목이 들어가는 영역입니다. 제목이 들어가는 영역입니다.</div>
+            <div class="desc">내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다.내용이 들어가는 영역입니다. 내용이 들어가는 영역입니다...</div>
+        </div>
+    </a>
+</div>
---
+++ img-txt/story.php
@@ -0,0 +1,13 @@
+<? include "../include/header.php"; ?>
+
+<div id="container">	
+	<div id="contArea">
+		<div class="real-cont">
+			<!--// content -->
+			
+			<!-- content //-->
+		</div>
+	</div>
+</div>
+
+<? include "../include/footer.php"; ?>
